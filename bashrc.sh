56335816c80008fb378fb35b403aff8684ec6c1b
James Bennett <james@b-list.org>
2009-09-06 12:47:53 -0500

    Various random updates I'd forgotten to commit.

--- bashrc.sh
+++ bashrc.sh
@@ -1,30 +1,37 @@
 #!/bin/bash
 
-alias ls="ls -G"
-
 # I like confirming destructive operations.
 alias cp="cp -iv"
 alias mv="mv -iv"
 alias rm="rm -iv"
 
-alias ljmail="/opt/local/bin/mutt -F ~/.mutt/configs/lj"
-alias blistmail="/opt/local/bin/mutt -F ~/.mutt/configs/blist"
+# Colored output from ls is nice, but the default color choices need
+# work for use on a dark/partially transparent background. Directories
+# stay blue but get bold, and symbolic links get bold cyan instead of
+# magenta.
+export CLICOLOR=1
+export LSCOLORS="ExGxcxdxbxegedabagacad"
 
 # Lots of path stuff because of all the random crap I have installed.
-export PATH=/opt/local/bin:/sw/bin:/usr/local/bin:$HOME/bin:$HOME/dev/django/svn/django/branches/releases/1.0.X/django/bin:$PATH
+export PATH=/opt/local/bin:/usr/local/bin:$HOME/bin:$HOME/dev/django/svn/django/trunk/django/bin:$PATH
 export PYTHONPATH=$HOME/dev/python-local:$PYTHONPATH
 
 # Hooray Emacs!
 export EDITOR=emacs
 
-# I like boring command prompts.
-export PS1="[\u@\h:\w]$ "
+# Prompt is not as complicated as the format string would
+# suggest. This one does:
+#
+# (time) user@host:dir $
+#
+# with user/host in green and working directory cyan.
+export PS1="(\[\e[0;37m\]\A\[\e[0;37m\]) \[\e[0;32m\]\u@\h\[\e[0;37m\]:\[\e[0;36m\]\w\[\e[0;37m\] \$ "
 
-# Hooray virtualenv!
+# Env variables and functions for virtualenvwrapper.
 export WORKON_HOME=$HOME/dev/virtualenvs
 . $HOME/bin/virtualenvwrapper_bashrc
 
-# Hooray custom completion!
+# Some useful additional completion.
 . $HOME/dev/django/svn/django/trunk/extras/django_bash_completion
 . $HOME/bin/hg_completion
 
