16dc34edc20f6c010bfaa27ac29ea3fb4b1fb3c4
James Bennett <james@b-list.org>
2012-05-17 03:38:30 -0500

    Check in latest versions.

--- bashrc.sh
+++ bashrc.sh
@@ -4,9 +4,10 @@
 alias cp="cp -iv"
 alias mv="mv -iv"
 alias rm="rm -iv"
+alias git=hub
 
 # I have multiple Pythons, but I want pydoc to give me docs for 2.6.
-alias pydoc='pydoc2.6'
+#alias pydoc='pydoc2.6'
 
 # Colored output from ls is nice, but the default color choices need
 # work for use on a dark/partially transparent background. Directories
@@ -16,7 +17,7 @@ export CLICOLOR=1
 export LSCOLORS="ExGxcxdxbxegedabagacad"
 
 # Lots of path stuff because of all the random crap I have installed.
-export PATH=/opt/local/bin:/usr/local/git/bin:/usr/local/bin:$HOME/bin:$HOME/dev/django/svn/django/trunk/django/bin:$PATH
+export PATH=/usr/bin:/usr/local/bin:$HOME/bin:$HOME/dev/django/svn/django/trunk/django/bin:$HOME/dev/git/mozilla/git-tools:$PATH
 export PYTHONPATH=$HOME/dev/python-local:$PYTHONPATH
 
 # Hooray Emacs!
@@ -31,12 +32,15 @@ export EDITOR=emacs
 export PS1="(\[\e[0;37m\]\A\[\e[0;37m\]) \[\e[0;32m\]\u@\h\[\e[0;37m\]:\[\e[0;36m\]\w\[\e[0;37m\] \$ "
 
 # Env variables and functions for virtualenvwrapper.
+export BUGZILLA_API=api-dev.bugzilla.mozilla.org
 export WORKON_HOME=$HOME/dev/virtualenvs
-. $HOME/bin/virtualenvwrapper_bashrc
+. $HOME/bin/virtualenvwrapper.sh
 
 # Some useful additional completion.
 . $HOME/dev/django/svn/django/trunk/extras/django_bash_completion
 . $HOME/bin/hg_completion
+. /usr/local/git/contrib/completion/git-completion.bash
 
-# Sekrit work stuffs
-. $HOME/bin/wol_deploy_config
+# Rackspace management fun.
+. $HOME/.secrets
+. $HOME/bin/rackspace.sh
