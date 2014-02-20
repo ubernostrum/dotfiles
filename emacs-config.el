a8fba48fdf1d1107432ddf4cfd48ba7d6704f8b3
James Bennett <james@b-list.org>
2014-02-20 14:28:17 -0600

    Random update.

--- emacs-config.el
+++ emacs-config.el
@@ -1,3 +1,4 @@
+
 ;;----------------------
 ;; General config stuff.
 ;;----------------------
@@ -8,7 +9,7 @@
 ;; No scroll bar, menu bar, toolbar or splash screen.
 (if (fboundp 'scroll-bar-mode) (scroll-bar-mode nil))
 (if (fboundp 'tool-bar-mode) (tool-bar-mode nil))
-(if (fboundp 'menu-bar-mode) (menu-bar-mode nil))
+(menu-bar-mode -1)
 (setq inhibit-startup-message t)
 
 ;; Don't crap backup files all over the place.
@@ -56,20 +57,18 @@
 
 (add-to-list 'load-path (expand-file-name "~/.elisp"))
 
+;; Theme.
+(add-to-list 'custom-theme-load-path "~/.elisp/emacs-color-theme-solarized")
+(load-theme 'solarized-dark t)
+
 ;; Custom Python mode.
 (load "python-mode")
-
-;; Programming the way Guido indented it.
-(setq indent-tabs-mode nil)
-
-;; Custom JavaScript mode.
-(autoload 'javascript-mode "javascript" nil t)
-(add-to-list 'auto-mode-alist '("\\.js\\'" . javascript-mode))
 
 ;; Custom CSS mode.
 (autoload 'css-mode "css-mode" nil t)
 (add-to-list 'auto-mode-alist '("\\.css$" . css-mode))
 
+;; C# mode.
 (autoload 'csharp-mode "csharp-mode" nil t)
 (add-to-list 'auto-mode-alist '("\\.cs$" . csharp-mode))
 
@@ -79,12 +78,6 @@
 (add-to-list 'auto-mode-alist '("\\.txt$" . rst-mode))
 (setq rst-level-face-base-light 0)
 (add-hook 'rst-mode-hook 'rst-text-mode-bindings)
-
-;; hg is my DVCS of choice.
-(load "mercurial")
-
-;; Speaking of version-control, I have a custom SVN install too.
-(setq vc-svn-program "/opt/local/bin/svn")
 
 
 ;;---------------
@@ -100,6 +93,12 @@
 ;; Yes, I sometimes work with large files. That's OK.
 (setq large-file-warning-threshold nil)
 
+;; Yes, I symlink things. Deal with it.
+(setq vc-follow-symlinks t)
+
+;; Programming the way Guido indented it.
+(setq indent-tabs-mode nil)
+
 ;; 80-character lines.
 (setq fill-column 79)
 
@@ -113,15 +112,21 @@
 ;;-------------------------------------------------------
 
 (custom-set-faces
-  ;; custom-set-faces was added by Custom.
-  ;; If you edit it by hand, you could mess it up, so be careful.
-  ;; Your init file should contain only one such instance.
-  ;; If there is more than one, they won't work right.
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
  '(default ((t (:stipple nil :foreground "white" :inverse-video nil :box nil :strike-through nil :overline nil :underline nil :slant normal :weight normal :height 115 :width normal :family "andale"))))
  '(mode-line ((t (:background "white" :foreground "black")))))
-(custom-set-variables
+;(custom-set-variables
   ;; custom-set-variables was added by Custom.
   ;; If you edit it by hand, you could mess it up, so be careful.
   ;; Your init file should contain only one such instance.
   ;; If there is more than one, they won't work right.
- '(rst-level-face-base-color "black"))
+; '(rst-level-face-base-color "black"))
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(custom-safe-themes (quote ("1e7e097ec8cb1f8c3a912d7e1e0331caeed49fef6cff220be63bd2a6ba4cc365" "a71be4e5e9e418025daea651f8a1628953abb7af505da5e556e95061b6a6e389" default))))
